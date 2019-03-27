9040c29f63dedfc589185927633cd706059e742a
Varun Wahi <varun27wahi@gmail.com>
2019-03-27 16:14:18 +0530

    Added verify page route

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const routes = require('next-routes')();
 
 routes
   .add('/issuers/:address/certificates/view', '/issuers/certificates/view')
-  .add('/issuers/:address/certificates/new', '/issuers/certificates/new');
+  .add('/issuers/:address/certificates/new', '/issuers/certificates/new')
+  .add('/issuers/:address/certificates/verify/:id', '/issuers/certificates/verify');
 
 module.exports = routes;
