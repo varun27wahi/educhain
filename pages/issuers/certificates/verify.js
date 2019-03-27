a8b5ca17c3745ec25ff448948f2492f1e0e3a0a3
Varun Wahi <varun27wahi@gmail.com>
2019-03-28 01:28:01 +0530

    Added verification functionality

--- pages/issuers/certificates/verify.js
+++ pages/issuers/certificates/verify.js
@@ -1,20 +1,77 @@
 import React, { Component } from 'react';
 import Layout from '../../../components/Layout';
-import { Table } from 'semantic-ui-react';
+import { Table, Message } from 'semantic-ui-react';
+import Issuer from '../../../ethereum/issuer';
 import { Link, Router } from '../../../routes';
 
 class VerifyCertificate extends Component {
   static async getInitialProps(props) {
     const { address, id } = props.query;
-    console.log(id);
 
-    return { id };
+    const issuer = Issuer(address);
+    const issuerName = await issuer.methods.issuerName().call();
+    const certificate = await issuer.methods.certificates(id).call();
+
+    return { certificate, address, id, issuerName };
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body, Cell } = Table;
+    const { certificate, address, id, issuerName } = this.props;
+
     return (
       <Layout>
-        <h3>Verify Certificate</h3>
+        <Link route={`/issuers/${address}/certificates/view`}>
+         <a>
+          Back to List
+         </a>
+        </Link>
+        <h3>Verify Certificate for {issuerName}</h3>
+
+        <Table basic="very" celled collapsing>
+          <Header>
+            <HeaderCell textAlign="center">Property</HeaderCell>
+            <HeaderCell>Value</HeaderCell>
+          </Header>
+
+          <Body>
+            <Row>
+              <Cell active textAlign="center">Unique Certificate ID</Cell>
+              <Cell>{certificate.id}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Description</Cell>
+              <Cell>{certificate.description}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Issuing Authority</Cell>
+              <Cell>{certificate.issuingAuthority}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Recipient ID</Cell>
+              <Cell>{certificate.recipientID}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Issuing Date</Cell>
+              <Cell>{certificate.issuingDate}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Type of Certificate</Cell>
+              <Cell>{certificate.typeOfCertificate}</Cell>
+            </Row>
+            <Row>
+              <Cell active textAlign="center">Additional Details</Cell>
+              <Cell>{certificate.details}</Cell>
+            </Row>
+          </Body>
+        </Table>
+
+        <Message
+          info
+          icon="info"
+          header="How to Verify!"
+          content="To verify the certificate, match the ID given here to the one given by the student."
+        />
       </Layout>
     );
   }
