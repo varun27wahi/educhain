2a4a45d78b8106e593b3eeb9ce960684fc3c41db
Varun Wahi <varun27wahi@gmail.com>
2019-03-28 14:41:45 +0530

    fresh factory contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import IssuerFactory from './build/IssuerFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(IssuerFactory.interface),
-  '0x002491558B8080B350acF85c42930929a879f7AA'
+  '0x79a15aBc48F41ffeBafE079E6F04f3a70cc7CA98'
 );
 
 export default instance;
